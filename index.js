d2a89d6f482092be784ed1331bc20625e93cd92d
123rishujha <jharishu796@gmail.com>
2025-05-06 17:57:30 +0530

    coding .

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+//connection with mongodb database;
+const { connection } = require("./config/db");
+//routes
+const { userRouter } = require("./routes/userRoutes");
+const { userLibraryRouter } = require("./routes/userLibraryRouter");
+const { awsRouter } = require("./routes/awsRoutes");
+const { authMiddleware } = require("./middlewares/authMiddleware");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: `${process.env.CLIENT_URL}`,
+    // origin: "http://localhost:5173",
+    // origin: "*",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", authMiddleware, (req, res) => {
+  res.json({ message: "working" });
+});
+
+//routes
+app.use("/api/user", userRouter);
+app.use("/api/library", userLibraryRouter);
+app.use("/api", awsRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "something went wrong";
+  res.status(statusCode).json({
+    status: statusCode,
+    msg: message,
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, async () => {
+  console.log("server started at http://localhost:8080...");
+  try {
+    await connection;
+    console.log("connected to database");
+  } catch (err) {
+    console.log(err);
+    console.log("Not connected to database");
+  }
+});
---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: {
+      type: mongoose.Schema.Types.Mixed,
+      required: true,
+    },
+    photo: { type: String },
+    role: { type: String, default: "user" },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = {
+  UserModel,
+};
